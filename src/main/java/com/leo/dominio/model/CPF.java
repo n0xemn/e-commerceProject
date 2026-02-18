c92837eca0c5b57eb6b3ba7d34be4a6ab8935005
Leonardo <leornado.silva4224@gmail.com>
2026-02-18 16:47:49 -0300

    Classe do CPF pronta com todas as verificações, usei a biblioteca Caelum Stella pra fazer as validaçoes.

--- src/main/java/com/leo/dominio/model/CPF.java
+++ src/main/java/com/leo/dominio/model/CPF.java
@@ -1,14 +1,48 @@
 package com.leo.dominio.model;
 
+import java.util.Objects;
+
+// pra validação de cpf
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+
 public final class CPF {
     private final String valor;
 
     public CPF(String cpf){
         if (cpf == null || cpf.isBlank()) {
-            throw new IllegalArgumentException("O cpf não pode ser nulo ou vazi.");
+            throw new IllegalArgumentException("O cpf não pode ser nulo ou vazio.");
+        }
+
+        cpf = reformatar(cpf);
+
+        if (! testarTamanho(cpf)) {
+            throw new IllegalArgumentException("Tamanho incorreto de cpf, deve ter 11 números.");
+        }
+
+        if (! validarCPF(cpf)) {
+            throw new IllegalArgumentException("CPF inválido.");
         }
 
         this.valor = cpf;
+    }
+
+    public boolean testarTamanho(String cpf){
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean validarCPF(String cpf){ 
+        CPFValidator cpfValidator = new CPFValidator();
+        try {
+            cpfValidator.assertValid(cpf);
+            return true;
+        } catch (InvalidStateException e) {
+            return false;
+        }
     }
 
     @Override
@@ -30,11 +64,11 @@ public final class CPF {
         return Objects.hash(valor);
     }
 
+    public String reformatar(String cpf){
+        return cpf.replaceAll("\\D", ""); // \D todo caracter que não estaja entre 0-9. \d é todo caracter que esteja entre 0-9
+    }
+    
     public String getValor() {
         return valor;
-    }
-
-    public String reformatar(String cpf){
-        return cpf.replaceAll("\\D", "");
     }
 }
