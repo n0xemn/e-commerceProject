ae51dc2b9dacd1c024e2af973d4d2443155baad0
Leonardo <leornado.silva4224@gmail.com>
2026-02-25 16:07:14 -0300

    Criado o enun moeda, e mudanças feitas em money quanto a isso.

---
+++ src/main/java/com/leo/dominio/shared/Moeda.java
@@ -0,0 +1,8 @@
+package com.leo.dominio.shared;
+
+public enum Moeda { 
+    BRL,
+    USD,
+    EUR
+    // talvez eu coloque mais depois, preguiça de pesquisar
+}
--- src/main/java/com/leo/dominio/shared/Money.java
+++ src/main/java/com/leo/dominio/shared/Money.java
@@ -5,9 +5,22 @@ import java.math.RoundingMode;
 
 public class Money {
     private final BigDecimal valor;
-    private final String moeda; // TODO mudar pra uma classe de moeda depois
+    private final Moeda moeda;
 
     public Money(BigDecimal valor, String moeda){
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) == 0 || valor.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("O valor não pode ser nulo e deve ser maior que zero.");
+        }
+        if (moeda == null || moeda.isBlank()) {
+            throw new IllegalArgumentException("O tipo de moeda não pode ser nulo.");
+        }
+
+        this.valor = valor.setScale(2, RoundingMode.HALF_EVEN);
+        this.moeda = Moeda.valueOf(moeda.toUpperCase());
+    }
+
+    // vai que né, nunca se sabe, talvez eu use mais esse
+    public Money(BigDecimal valor, Moeda moeda){
         if (valor == null || valor.compareTo(BigDecimal.ZERO) == 0 || valor.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("O valor não pode ser nulo e deve ser maior que zero.");
         }
@@ -21,6 +34,20 @@ public class Money {
 
     // fiz dois pq vai que precisa, se não eu apago um dos dois
     public Money(String valor, String moeda){
+        BigDecimal newDindin = new BigDecimal(valor);
+
+        if (newDindin == null || newDindin.compareTo(BigDecimal.ZERO) == 0 || newDindin.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("O valor não pode ser nulo e deve ser maior que zero.");
+        }
+        if (moeda == null) {
+            throw new IllegalArgumentException("O tipo de moeda não pode ser nulo.");
+        }
+
+        this.valor = newDindin.setScale(2, RoundingMode.HALF_EVEN);
+        this.moeda = Moeda.valueOf(moeda.toUpperCase());
+    }
+
+    public Money(String valor, Moeda moeda){
         BigDecimal newDindin = new BigDecimal(valor);
 
         if (newDindin == null || newDindin.compareTo(BigDecimal.ZERO) == 0 || newDindin.compareTo(BigDecimal.ZERO) < 0) {
@@ -64,7 +91,7 @@ public class Money {
         return this.valor;
     }
 
-    public String getMoeda(){ // TODO mudar retorno 
+    public Moeda getMoeda(){ // TODO mudar retorno 
         return this.moeda;
     }
 
@@ -79,6 +106,6 @@ public class Money {
         }
 
         Money comparar = (Money) e;
-        return this.valor.compareTo(comparar.getValor()) == 0 && this.moeda.equals(comparar.getMoeda());
+        return this.valor.compareTo(comparar.getValor()) == 0 && this.moeda == comparar.getMoeda();
     }
 }
